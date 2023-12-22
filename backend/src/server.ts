6fa9ffd9d70252c2b3b02e40b76233e7e6546a78
Aniket Satapathy <aniketsatapathy193@gmail.com>
2023-12-22 21:33:50 +0530

    [Backend]New URL

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,40 @@
-console.log('Hello World!');
+import express, {Application} from 'express';
+import { PORT } from "./config/secrets";
+
+
+class Server {
+    private app: Application;
+    private readonly port: number;
+
+    // Routes
+    // private readonly apiDocsRoute = '/api/docs';
+    // private readonly authRoute = '/api/auth';
+    // private readonly userRoute = '/api/user';
+
+    constructor() {
+        this.app = express();
+        this.port = PORT;
+    }
+
+    basicMiddleware() {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: true}));
+    }
+
+    routes() {
+        this.app.use('/', (req, res) => {
+           res.send("Hello World")
+        });
+    }
+
+    start() {
+        this.app.listen(this.port, () => {
+            console.log(`Server listening on port ${this.port}`);
+        });
+    }
+}
+
+const server = new Server();
+server.basicMiddleware();
+server.routes();
+server.start();
