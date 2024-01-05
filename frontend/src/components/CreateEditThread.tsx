8859590f0e639316a5781eeb9b9fd8e7d7433fdd
Aniket Satapathy <aniketsatapathy193@gmail.com>
2024-01-05 16:31:08 +0530

    [Frontend]Bugs fixed

--- frontend/src/components/CreateEditThread.tsx
+++ frontend/src/components/CreateEditThread.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import {
     Modal,
     ModalContent,
@@ -7,29 +7,29 @@ import {
     ModalFooter,
     Button,
     useDisclosure,
-    Textarea
+    // Textarea
 } from "@nextui-org/react";
 
 
-const ThreadForm = () => {
-    return (
-        <div className="flex flex-col gap-4">
-            <Textarea
-                className="w-full"
-                placeholder="What's happening?"
-                style={{width:"100%"}}
-                rows={4}
-            />
-            <Button fullWidth={false} style={{width:"fit-content"}} color="success" endContent={<CameraIcon fill="currentColor" className="w-5 h-5" />}>
-                Add Media
-            </Button>
-        </div>
-    );
-}
+// const ThreadForm = () => {
+//     return (
+//         <div className="flex flex-col gap-4">
+//             <Textarea
+//                 className="w-full"
+//                 placeholder="What's happening?"
+//                 style={{width:"100%"}}
+//                 rows={4}
+//             />
+//             <Button fullWidth={false} style={{width:"fit-content"}} color="success" endContent={<CameraIcon fill="currentColor" className="w-5 h-5" />}>
+//                 Add Media
+//             </Button>
+//         </div>
+//     );
+// }
 export default function CreateEditThread() {
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
-    const [threads, setThreads] = React.useState([])
+    // const [threads, setThreads] = React.useState([])
 
     return (
         <>
@@ -40,15 +40,14 @@ export default function CreateEditThread() {
                         <>
                             <ModalHeader className="flex flex-col gap-1">Create A New Thread</ModalHeader>
                             <ModalBody>
-                                {
-                                    threads.map((thread, index) => {
-                                        return (
-                                            <ThreadForm key={`${index}`} />
-                                        )
-                                    })
-                                }
-
-                                <Button color="success" variant="light" onPress={() => setThreads([...threads, <ThreadForm />])}>
+                                {/*{*/}
+                                {/*    threads.map((thread, index) => {*/}
+                                {/*        return (*/}
+                                {/*            <ThreadForm key={`${index}`} />*/}
+                                {/*        )*/}
+                                {/*    })*/}
+                                {/*}*/}
+                                <Button color="success" variant="light" onPress={() => console.log('x')}>
                                     Add Thread
                                 </Button>
                             </ModalBody>
@@ -69,23 +68,23 @@ export default function CreateEditThread() {
 }
 
 
-const CameraIcon = ({fill = 'currentColor', ...props}) => {
-    return (
-        <svg
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            {...props}
-        >
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M17.44 6.236c.04.07.11.12.2.12 2.4 0 4.36 1.958 4.36 4.355v5.934A4.368 4.368 0 0117.64 21H6.36A4.361 4.361 0 012 16.645V10.71a4.361 4.361 0 014.36-4.355c.08 0 .16-.04.19-.12l.06-.12.106-.222a97.79 97.79 0 01.714-1.486C7.89 3.51 8.67 3.01 9.64 3h4.71c.97.01 1.76.51 2.22 1.408.157.315.397.822.629 1.31l.141.299.1.22zm-.73 3.836c0 .5.4.9.9.9s.91-.4.91-.9-.41-.909-.91-.909-.9.41-.9.91zm-6.44 1.548c.47-.47 1.08-.719 1.73-.719.65 0 1.26.25 1.72.71.46.459.71 1.068.71 1.717A2.438 2.438 0 0112 15.756c-.65 0-1.26-.25-1.72-.71a2.408 2.408 0 01-.71-1.717v-.01c-.01-.63.24-1.24.7-1.699zm4.5 4.485a3.91 3.91 0 01-2.77 1.15 3.921 3.921 0 01-3.93-3.926 3.865 3.865 0 011.14-2.767A3.921 3.921 0 0112 9.402c1.05 0 2.04.41 2.78 1.15.74.749 1.15 1.738 1.15 2.777a3.958 3.958 0 01-1.16 2.776z"
-                fill={fill}
-            />
-        </svg>
-    );
-};
+// const CameraIcon = ({fill = 'currentColor', ...props}) => {
+//     return (
+//         <svg
+//             width={24}
+//             height={24}
+//             viewBox="0 0 24 24"
+//             fill="none"
+//             xmlns="http://www.w3.org/2000/svg"
+//             {...props}
+//         >
+//             <path
+//                 fillRule="evenodd"
+//                 clipRule="evenodd"
+//                 d="M17.44 6.236c.04.07.11.12.2.12 2.4 0 4.36 1.958 4.36 4.355v5.934A4.368 4.368 0 0117.64 21H6.36A4.361 4.361 0 012 16.645V10.71a4.361 4.361 0 014.36-4.355c.08 0 .16-.04.19-.12l.06-.12.106-.222a97.79 97.79 0 01.714-1.486C7.89 3.51 8.67 3.01 9.64 3h4.71c.97.01 1.76.51 2.22 1.408.157.315.397.822.629 1.31l.141.299.1.22zm-.73 3.836c0 .5.4.9.9.9s.91-.4.91-.9-.41-.909-.91-.909-.9.41-.9.91zm-6.44 1.548c.47-.47 1.08-.719 1.73-.719.65 0 1.26.25 1.72.71.46.459.71 1.068.71 1.717A2.438 2.438 0 0112 15.756c-.65 0-1.26-.25-1.72-.71a2.408 2.408 0 01-.71-1.717v-.01c-.01-.63.24-1.24.7-1.699zm4.5 4.485a3.91 3.91 0 01-2.77 1.15 3.921 3.921 0 01-3.93-3.926 3.865 3.865 0 011.14-2.767A3.921 3.921 0 0112 9.402c1.05 0 2.04.41 2.78 1.15.74.749 1.15 1.738 1.15 2.777a3.958 3.958 0 01-1.16 2.776z"
+//                 fill={fill}
+//             />
+//         </svg>
+//     );
+// };
 
