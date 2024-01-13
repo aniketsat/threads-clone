e3e7e04f4c11725ebcac004e4cfc773fefbc56b6
Aniket Satapathy <aniketsatapathy193@gmail.com>
2024-01-13 11:46:33 +0530

    [Backend]bugs fixed

--- backend/dist/config/secrets.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-exports.PORT = parseInt(process.env.PORT, 10) || 8000;
